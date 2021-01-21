34348ae55ef57b58b60053f10b886d5dbd628d38
SageTengu <kyle.kleven@outlook.com>
2021-01-20 23:55:56 -0600

    Created and finished clearSchedule and colorChanger functions

--- 05-Third-Party-APIs/Develop/script.js
+++ 05-Third-Party-APIs/Develop/script.js
@@ -4,7 +4,7 @@
 // Create a list of 9 time slots (9am-5pm) 
 // Each row contains a text display, event entry field, and save button (9 rows 3 columns)
 // Use insert row to build the schedule?
-    
+
 
 //ROWS
 // Display hardcoded times/text in col 1
@@ -23,43 +23,154 @@
 // create JSON parser to store object to local storage
 // when save is clicked, save corresponding text field value to local storage
 
-$('#currentDay').text((moment().format('LLLL')));
+var currentTime = moment().format('LLLL');
 
-var schedule = [];
+$('#currentDay').text(currentTime);
 
-$('.save').on('click', function() {
-    var savedEvent = $('.activity').val().trim();
-    
-    if (savedEvent == "") {
-        return;
+var schedule = [
+    {
+        time: "0900",
+        activity: ""
+    },
+    {
+        time: "1000",
+        activity: ""
+    },
+    {
+        time: "1100",
+        activity: ""
+    },
+    {
+        time: "1200",
+        activity: ""
+    },
+    {
+        time: "1300",
+        activity: ""
+    },
+    {
+        time: "1400",
+        activity: ""
+    },
+    {
+        time: "1500",
+        activity: ""
+    },
+    {
+        time: "1600",
+        activity: ""
+    },
+    {
+        time: "1700",
+        activity: ""
     }
-    
-    console.log(savedEvent);
-    
-    schedule.push(savedEvent);
-    
-    console.log(schedule);
+];
 
-    JSON.stringify(localStorage.setItem("schedule", savedEvent));
-    //ADD: push to array
-    // saveToArray();
-    // saveToStorage();
-    loadSchedule();
-});
+//
+function clearSchedule() {
+    sessionStorage.clear();
+    $('.activity').empty();
+}
 
-function loadSchedule() {
-    console.log("load schedule");
-
-    var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
-
-    for (var i = 0; i < schedule.length; i++) {
-        
-        if (storedSchedule !== null) {
-            savedEvent = storedSchedule;
-        }
-        
-        $('.activity').append(storedSchedule);
+//
+function displayTimes() {
+    for (var t = 0; t < schedule.length; t++) {
+        currentTime = moment().format('HH');
+        //
+        $('#time-' + t).text(schedule[t].time);
     }
 }
 
-loadSchedule();
+//
+function colorChanger() {
+
+    //    
+    for (var c = 0; c < schedule.length; c++) {
+        //
+        var d = $('#time-' + c).html();
+        var a = $('#activity-' + c);
+
+        //
+        d = schedule[c].time / 100;
+
+        //
+        if (d < currentTime) {
+            //
+            a.addClass('past');
+            a.removeClass('present');
+            a.removeClass('future');
+        }
+        else if (d > currentTime) {
+            //
+            a.removeClass('past');
+            a.removeClass('present');
+            a.addClass('future');
+        }
+        else {
+            //
+            a.removeClass('past');
+            a.addClass('present');
+            a.removeClass('future');
+        }
+    }
+
+}
+
+//
+function checkActivity() {
+    for (var a = 0; a < schedule.length; a++) {
+        //
+        var currentActivity = $('#activity-' + a).val().trim();
+        //
+        if (currentActivity == "") {
+            return;
+        }
+        //
+        schedule[a].activity = currentActivity;
+    }
+}
+
+//
+function saveSchedule() {
+    //
+    sessionStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+// 
+function renderSchedule() {
+    //
+    displayTimes();
+    colorChanger();
+    //
+    var lastSchedule = JSON.parse(sessionStorage.getItem("schedule"));
+    //
+    for (var r = 0; r < schedule.length; r++) {
+        //
+        if (lastSchedule !== null) {
+            //            
+            document.querySelector('#activity-' + r).innerHTML = lastSchedule[r].activity;
+        } else {
+            return;
+        }
+    }
+}
+
+//
+function init() {
+    renderSchedule();
+}
+
+$('#clear').on('click', function () {
+    clearSchedule();
+});
+
+//
+$('.save').on('click', function (event) {
+    event.preventDefault();
+    checkActivity();
+    saveSchedule();
+    renderSchedule();
+});
+
+//
+init();
